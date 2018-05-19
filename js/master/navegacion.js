d92bf45ba38eacef740a5c2abee0f4d558265e2a
val040413 <14240791@itleon.edu.mx>
2018-05-18 21:19:17 -0500

    Pantallas Master
    Agrego las pantallas de master y los cambios en la de usuario y administrador para poder ir a la pantalla principal

---
+++ js/master/navegacion.js
@@ -0,0 +1,37 @@
+
+/*
+    Nombre: Juan Pablo Gallardo Ochoa
+    Descripción: Función para estar navegando entrelas pantallas html
+    Fecha: 18 de Mayo del 2018
+*/
+
+$('#izq-contenedor').click(function(e){
+	var id = e.target.id;
+	if (id=="btn-registrar-empleados") {
+		window.location.assign("../master/registrarEmpleados.html");
+		
+    }
+
+    if (id=="btn-bempleados") {
+		window.location.assign("../master/busquedaEmpleados.html");
+    }
+    if (id=="btn-inicio") {
+		window.location.assign("../master/index.html");
+    }
+
+
+    if (id=="btn-salir") {
+		swal({
+			title: "¿Está seguro de que desea salir?",
+			text: "Una vez presionado 'OK' tendrá que volver a loguearse",
+			icon: "warning",
+			buttons: true,
+			dangerMode: true,
+		})
+		.then((willDelete) => {
+			if (willDelete) {
+				window.location.replace("../../index.html")
+			} 
+		});
+	}
+    });
